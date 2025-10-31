35ec3b79f35835b73cfb983fa9948e4499f05591
JMc <jkmendyka@gmail.com>
2025-10-31 23:55:00 +0100

    add RemovedFiles property to ScanResultDto

--- Core/Application/Models/ScanResultDto.cs
+++ Core/Application/Models/ScanResultDto.cs
@@ -4,6 +4,7 @@ public class ScanResultDto
 {
     public int NewFiles { get; set; }
     public int UpdatedFiles { get; set; }
+    public int RemovedFiles { get; set; }
     public int ErrorFiles { get; set; }
     public TimeSpan Duration { get; set; }
     public List<string> Errors { get; set; } = new();
