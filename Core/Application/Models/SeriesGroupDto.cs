80ebd44679f32bee0fa36c21d845f957090fceb0
JMc <jkmendyka@gmail.com>
2025-11-06 21:17:46 +0100

    refactor UI components and models: initialize collections with array syntax, simplify boolean properties, and remove unused automation and spinner code

--- Core/Application/Models/SeriesGroupDto.cs
+++ Core/Application/Models/SeriesGroupDto.cs
@@ -3,7 +3,7 @@ namespace Translarr.Core.Application.Models;
 public class SeriesGroupDto
 {
     public required string SeriesName { get; set; }
-    public List<SeasonGroupDto> Seasons { get; set; } = new();
+    public List<SeasonGroupDto> Seasons { get; set; } = [];
     public int TotalFiles { get; set; }
     public int WantedFiles { get; set; }
     public int ProcessedFiles { get; set; }
