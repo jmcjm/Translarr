4f702f368f218a82ffcd3349b66c4b7ca00adfde
JMc <jkmendyka@gmail.com>
2026-03-21 02:53:56 +0100

    Add missing AddAuthentication() to WebApp - required by authorization middleware

--- Frontend/HavitWebApp/Program.cs
+++ Frontend/HavitWebApp/Program.cs
@@ -29,6 +29,7 @@ public class Program
         builder.Services.AddScoped<AuthCookieHolder>();
         builder.Services.AddScoped<AuthenticatedApiClientFactory>();
         builder.Services.AddScoped<AuthenticationStateProvider, TranslarrAuthStateProvider>();
+        builder.Services.AddAuthentication();
         builder.Services.AddAuthorization(options =>
         {
             options.FallbackPolicy = new Microsoft.AspNetCore.Authorization.AuthorizationPolicyBuilder()
